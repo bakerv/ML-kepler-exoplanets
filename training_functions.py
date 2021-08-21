b85a6afd2c3ba210f7527fff448ff58aa9aa8474
bakerv <80018640+bakerv@users.noreply.github.com>
2021-08-21 04:45:49 -0500

    Achieved 95% validation accuracy after training on 33 columns

--- training_functions.py
+++ training_functions.py
@@ -5,36 +5,41 @@ import tensorflow_addons as tfa
 
 class CustomHyperband(kt.Hyperband):
     def run_trial(self, trial, *args, **kwargs):
-        kwargs['batch_size'] = trial.hyper_parameters.Choice(
+        kwargs['batch_size'] = trial.hyperparameters.Choice(
             'batch_size',
-            values = [17, 85, 221, 663, 1326])
+            values = [17])
         super(CustomHyperband, self).run_trial(trial, *args, **kwargs)   
     
 def model_tuner(hyper_parameters):
     # tuning parameters
-    dense_layer_1 = hyper_parameters.Choice('dense_1',
-                                      values = [32, 64, 128, 
-                                                256, 320])
-    dense_layer_2 = hyper_parameters.Choice('dense_2',
-                                      values = [32, 64, 128, 
-                                                256, 320])
-    test_learning_rates = hyper_parameters.Choice('learning_rate',
-                                                  values = [ 0.001, 0.01,
-                                                            0.05, 0.1])
-    
+    look_ahead_sync = hyperparameters.Choice('sync',
+                                             values=[3,6,9])
+    look_ahead_step_size = hyperparameters.Choice('stepsize',
+                                                  values = [0.1,0.3,
+                                                            0.5,0.9])
     rectified_adam = tfa.optimizers.RectifiedAdam(
         learning_rate = test_learning_rates)
     ranger = tfa.optimizers.Lookahead(rectified_adam,
-                                      sync_period = 6,
-                                      slow_step_size = 0.5)
+                                      sync_period = look_ahead_sync,
+                                      slow_step_size = look_ahead_step_size)
     mish = tfa.activations.mish
-
+    dense_layer_1 = hyper_parameters.Choice('dense_1',
+                                            values = [64, 128])
+    drop_out_rate = hyper_parameters.Choice('dropout',
+                                            values= 0.4,0.5,0.6)
+    dense_layer_2 = hyper_parameters.Choice('dense_2',
+                                            values = [32, 64, 128, 
+                                                      256, 320])
+    test_learning_rates = hyper_parameters.Choice('learning_rate',
+                                                  values = [1e-1, 1e-2, 4e-2,
+                                                            5e-2, 1e-3, 1e-4])
+    
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(units = dense_layer1, activation = mish, input_dim=33),
-        tf.keras.layers.Dense(units = dense_layer2, activation = mish),
-        tf.keras.layers.Dense(1, activation = mish)
+        tf.keras.layers.Dense(units = dense_layer_1, activation = mish, input_dim=33),
+        tf.keras.layers.Dropout(rate = drop_out_rate)
+        tf.keras.layers.Dense(units = dense_layer_2, activation = mish),
+        tf.keras.layers.Dense(1, activation = 'sigmoid')
     ])
-
     model.compile(
         optimizer = ranger,
         loss = 'binary_crossentropy',

4b84cb78b13889210823a1583c21247821529c82
bakerv <80018640+bakerv@users.noreply.github.com>
2021-08-24 02:53:07 -0500

    Begun analysis of the results, updating readme and notebooks

--- training_functions.py
+++ training_functions.py
@@ -15,17 +15,24 @@ def model_tuner(hyper_parameters):
     test_learning_rates = hyper_parameters.Choice('learning_rate',
                                               values = [1e-1, 1e-2, 4e-2,
                                                         5e-2, 1e-3, 1e-4])
-    look_ahead_sync = hyper_parameters.Choice('sync',
-                                             values=[3,6,9])
-    look_ahead_step_size = hyper_parameters.Choice('stepsize',
-                                                  values = [0.1,0.3,
-                                                            0.5,0.9])
-    rectified_adam = tfa.optimizers.RectifiedAdam(
-        learning_rate = test_learning_rates)
-    ranger = tfa.optimizers.Lookahead(rectified_adam,
-                                      sync_period = look_ahead_sync,
-                                      slow_step_size = look_ahead_step_size)
-    mish = tfa.activations.mish
+    # parameters for SGD
+    nesterov_condition = hyper_parameters.Choice('nesterov',
+                                                  values = [True, False])
+    test_momentum_rates = hyper_parameters.Choice('momentum',
+                                                  values = [0.5, 0.6, 0.8,               
+                                                            0.9, 0.95])
+    # parameters and definitions for mish and Ranger
+    #look_ahead_sync = hyper_parameters.Choice('sync',
+      #                                       values=[3,6,9])
+   # look_ahead_step_size = hyper_parameters.Choice('stepsize',
+    #                                              values = [0.1,0.3,
+    #                                                        0.5,0.9])
+   # rectified_adam = tfa.optimizers.RectifiedAdam(
+    #    learning_rate = test_learning_rates)
+   # ranger = tfa.optimizers.Lookahead(rectified_adam,
+      #                                sync_period = look_ahead_sync,
+      #                                slow_step_size = look_ahead_step_size)
+    #mish = tfa.activations.mish
     dense_layer_1 = hyper_parameters.Choice('dense_1',
                                             values = [64, 128])
     drop_out_rate = hyper_parameters.Choice('dropout',
@@ -36,13 +43,15 @@ def model_tuner(hyper_parameters):
 
     
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(units = dense_layer_1, activation = mish, input_dim=32),
+        tf.keras.layers.Dense(units = dense_layer_1, activation = 'relu', input_dim=32),
         tf.keras.layers.Dropout(rate = drop_out_rate),
-        tf.keras.layers.Dense(units = dense_layer_2, activation = mish),
+        tf.keras.layers.Dense(units = dense_layer_2, activation = 'relu'),
         tf.keras.layers.Dense(1, activation = 'sigmoid')
     ])
     model.compile(
-        optimizer = ranger,
+        optimizer = tf.keras.optimizers.SGD(learning_rate = test_learning_rates,
+                                            momentum = test_momentum_rates,
+                                            nesterov = nesterov_condition),
         loss = 'binary_crossentropy',
         metrics = ['accuracy'])
     return model 
